dc921ffb5ec7816965c7a1e54daf1fb5c9c92e10
Tim van Dam <timvandamcs@gmail.com>
2021-12-17 06:27:06 +0100

    day 17

---
+++ 17.js
@@ -0,0 +1,134 @@
+pipe=(start=document.body.innerText)=>(...fns)=>fns.reduce((input, fn) => fn(input), start)
+function permutator(inputArr) {
+    var results = [];
+  
+    function permute(arr, memo) {
+      var cur, memo = memo || [];
+  
+      for (var i = 0; i < arr.length; i++) {
+        cur = arr.splice(i, 1);
+        if (arr.length === 0) {
+          results.push(memo.concat(cur));
+        }
+        permute(arr.slice(), memo.concat(cur));
+        arr.splice(i, 0, cur[0]);
+      }
+  
+      return results;
+    }
+  
+    return permute(inputArr);
+  }
+getNums = txt => txt.match(/(\d+)/g).map(Number)
+makeFreqArr = nums => nums.reduce((r, c) => ++r[c]&&r, Array(9).fill(0))
+repeat = (times, ...fns) => (input) => { for (let i = 0; i < times; i++) input = fns.reduce((input, fn) => fn(input)||input, input); return input }
+rotateLeft = (nums) => void(nums.push(nums.shift()))
+rotateRight = (nums) => void(nums.unshift(nums.pop()))
+sum=a=>a.reduce((x,y)=>x+y,0)
+arrwithdefault=(defaultval=()=>0)=>new Proxy([], {
+  get(a,i,r) {
+    for (let j = a.length; j < i; j++) r[j]; // set prev defaults
+    if (a[i] === undefined) {
+      a[i] = defaultval()
+    }
+    return a[i]
+  },
+  set(a,i,v,r) {
+    for (let j = a.length; j < i; j++) r[j]; // set prev defaults
+    a[i] =v
+  }
+})
+imat=(dims,defaultval=0)=>{
+  res=[()=>defaultval]
+  for (let i = 0; i < dims; i++) {
+    res.push(()=>arrwithdefault(res[i]))
+  }
+  return res[res.length-1]()
+}
+mat = (dims,fill=()=>0) => dims.length === 1 ? Array(dims[0]).fill(fill()):Array.from({length:dims.splice(-1)}, () => mat(dims,fill))
+zip = (a,...r)=> a.map((a,i)=>[a,...r.map(r=>r[i])])
+rng = (f, t) => Array.from({length:Math.abs(f-t)+1},(_,i)=>f+i*(f<t?1:-1))
+repeat=(arr,cnt)=>Array.from({length:cnt},()=>[...arr]).flat(1)
+forcelen=(arr,len)=>repeat(arr,Math.ceil(len/arr.length)).slice(0, len)
+
+f = {
+  numberlist: /(\d+)/g,
+  number: /(\d+)$/g,
+  digits: /(\d)/g,
+  chars: /(\w)/g,
+  things: /(.)/g,
+  rlines: '',//document.body.innerText.trim().split('\n'),
+  lines: ()=>f.rlines.map(e=>e.trim()).filter(e=>e),
+  read: (r=f.numberlist,lines=f.lines())=>lines.map(l=>[...l.matchAll(r)].map(e=>e.slice(1).map(e=>isNaN(e)?e:Number(e))).flat(Infinity)),
+  // reads matrix based on given size (height)
+  readmatsCS: (size,lines=f.lines()) => f.read(f.numberlist,lines).reduce((mats, row, i, a) => i%size==0?[...mats,Array.from({length:size},(_,j)=>a[i + j])]:mats, []),
+  // reads matrix based on empty lines
+  readmatsEL: (lines=f.rlines) => lines.reduce((r, line) => line.trim().length ? [r, r[1].push([...line.matchAll(f.numberlist)].map(e=>e.slice(1).map(e=>isNaN(e)?e:Number(e))).flat(Infinity))][0] : [r, r[0].push(r[1]), r[1] = []][0], [[], []])[0]
+}
+Set.prototype.union = function (...sets) {
+  return new Set([...this, ...sets.flatMap(set => [...set])])
+}
+Set.prototype.intersect = function (...sets) {
+  return new Set([...this].filter(e => sets.every(set => set.has(e))))
+}
+Set.prototype.difference = function (b) {
+  return new Set([...this].filter(e => !b.has(e)))
+}
+Set.prototype.xor = function (b) {
+  return this.union(this.difference(b), b.difference(this))
+}
+set = (...elements) => new Set(elements)
+
+
+b=`target area: x=70..125, y=-159..-121`.trim()
+// b=document.body.innerText
+nums = b.match(/-?\d+/g).map(Number)
+targetx = [nums[0], nums[1]].sort((x,y)=>x-y)
+targety = [nums[2], nums[3]].sort((x,y)=>x-y)
+xvel = 0 /// forward
+yvel = 0 // up
+
+// find the best initial velocity
+
+let maxy = 0
+let count = 0
+
+console.log(targetx)
+console.log(targety)
+
+for (let ax = -1000; ax < 1000; ax++) {
+    for (let ay = -1000; ay < 1000; ay++) {
+        let xvel = ax
+        let yvel = ay
+        let x = 0
+        let y = 0
+        let Y = 0
+        steps = 1000
+        for (let s = 0; s < steps; s++) {
+            x += xvel
+            y += yvel
+
+            if (x > targetx[1]) break
+            if (y < targety[0]) break
+        
+            xvel += xvel>0?-1:xvel<0?1:0
+            yvel -= 1
+
+            Y = Math.max(y, Y)
+        
+            if (x >= targetx[0] && x <= targetx[1] && y >= targety[0] && y <= targety[1]) {
+                // hit
+                // console.log('hit',x,y)
+                if (Y > maxy) {
+                    maxy = Y
+                }
+                count++
+                break
+            }
+        
+        }
+    }
+}
+console.log('done')
+console.log(maxy)
+console.log(count)
